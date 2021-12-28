53d7d433852cfd15d675f06f227aabc44339bb74
tomalessi <tom.alessi@gmail.com>
2021-12-28 11:32:46 -0800

    Update br.js

--- br.js
+++ br.js
@@ -1,16 +1,18 @@
 (function executeRule(current, previous /*null when async*/) {
 
+    var webhook_key = 'INSERT';  // StatusDashboard Webhook Key
     var debug = false;
+    var statusdashboard_endpoint = 'https://www.statusdashboard.com/webhooks/servicenow/';
 	
     try {
         var r = new sn_ws.RESTMessageV2();
-        r.setEndpoint("https://www.statusdashboard.com/webhooks/servicenow/<INSERT WEBHOOK KEY>/");
+        r.setEndpoint(statusdashboard_endpoint + webhook_key + '/');
         r.setHttpMethod("post");
 
-        var sys_id = current.getValue("sys_id");
-        var state = current.getValue("state");
-        var business_service = current.getValue("business_service");
-        var description = current.getValue("description");
+        var sys_id = current.getValue('sys_id');
+        var state = current.getValue('state');
+        var business_service = current.getValue('business_service');
+        var description = current.getValue('description');
         var impacted_services = [];
 
         // Get all impacted services
@@ -18,21 +20,21 @@
         gr.addQuery('task', current.sys_id);
         gr.query();
         while (gr.next()) {
-            gs.info("Related CI found (name): " + gr.cmdb_ci_service.name);
-            gs.info("Related CI found (sys_id): " + gr.cmdb_ci_service.sys_id);
+            if (debug == true) {gs.info('Related CI found (name): ' + gr.cmdb_ci_service.name);}
+            if (debug == true) {gs.info('Related CI found (sys_id): ' + gr.cmdb_ci_service.sys_id);}
             impacted_services.push(gr.cmdb_ci_service.sys_id.toString());
         }
 
         var obj = {
-            "sys_id": sys_id,
-            "state": state,
-            "business_service": business_service,
-            "description": description,
-            "impacted_services": impacted_services
+            'sys_id': sys_id,
+            'state': state,
+            'business_service': business_service,
+            'description': description,
+            'impacted_services': impacted_services
         };
 		
         var body = JSON.stringify(obj);
-        gs.info("Webhook body: " + body);
+        if (debug == true) {gs.info('Webhook body: ' + body);}
                
         // Add the body to the request
         r.setRequestBody(body);
@@ -41,9 +43,9 @@
         var httpStatus = response.getStatusCode();
     } catch (ex) {
         var message = ex.message;
-        gs.error("Error message: " + message);
+        gs.error('Error message: ' + message);
     }
 
-    gs.info("Webhook target HTTP status response: " + httpStatus);
+    if (debug == true) {gs.info('Webhook target HTTP status response: ' + httpStatus);}
 
 })(current, previous);
